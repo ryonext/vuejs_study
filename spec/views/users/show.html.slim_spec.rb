02d579d60821dccb6c79eeabe859dbd6927dd7c4
ryonext <ryonext.s@gmail.com>
2015-11-04 19:45:48 +0900

    Prepare to test vue.

---
+++ spec/views/users/show.html.slim_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "users/show", type: :view do
+  before(:each) do
+    @user = assign(:user, User.create!(
+      :name => "Name",
+      :age => 1,
+      :profile => "MyText"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/1/)
+    expect(rendered).to match(/MyText/)
+  end
+end
